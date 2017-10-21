b47dde31be4ffde8dc09023e6b8f125ead06b4b0
Shravika Mittal <shravika16093@iiitd.ac.in>
2017-10-21 13:11:27 +0530

    faculty page layout added

---
+++ java/faculty_Page.java
@@ -0,0 +1,59 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public	class faculty_Page extends Application	
+{	
+	public static void main(String[] args)	
+	{	
+		launch(args);	
+	}	
+	@Override	
+	public void start(Stage	primaryStage)	
+	{	
+		primaryStage.setTitle("Faculty Page");		
+		Button btn	= new Button("View Room");	
+		Button btn2 = new Button("Book Room");
+		Button btn3 = new Button("Cancel Request");
+		Button btn4 = new Button("Logout");
+		Label l1 = new Label("Name:");
+		Label l2 = new Label("Type:");
+		Label l3 = new Label("Courses Taken:");
+		
+		btn.setPrefHeight(80);
+		btn.setPrefWidth(150);
+		btn2.setPrefHeight(80);
+		btn2.setPrefWidth(150);
+		btn3.setPrefHeight(80);
+		btn3.setPrefWidth(150);
+		btn4.setPrefHeight(80);
+		btn4.setPrefWidth(300);
+		
+		GridPane root = new GridPane();
+		
+		root.setPadding(new Insets(20));
+	    root.setHgap(35);
+	    root.setVgap(15);
+		
+		root.getColumnConstraints().add(new ColumnConstraints(100));
+		root.getColumnConstraints().add(new ColumnConstraints(200));
+		root.getColumnConstraints().add(new ColumnConstraints(200));
+		
+		root.add(l1, 0, 0, 1, 1);
+		root.add(btn4, 3, 0, 1, 1);
+		root.add(l2, 0, 1, 1, 1);
+		root.add(btn, 2, 3, 1, 1);
+		root.add(btn2, 2, 4, 1, 1);
+		root.add(btn3, 2, 5, 1, 1);
+		root.add(l3, 0, 6, 1, 1);
+		
+		Scene scene	= new Scene(root, 900, 400);		
+		primaryStage.setScene(scene);		
+		primaryStage.show();		
+	}	
+}
