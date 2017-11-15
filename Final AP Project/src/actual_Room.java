ce22ecc445aa5210d7e21f4121fd82b7e7ff93d1
Shravika Mittal <shravika16093@iiitd.ac.in>
2017-11-15 23:56:57 +0530

    actual_Room documenation

--- Final AP Project/src/actual_Room.java
+++ Final AP Project/src/actual_Room.java
@@ -1,29 +1,39 @@
 import java.io.Serializable;
-import java.lang.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * actual_Room.java contains the attributes that each room object should have
+ * 
+ * It also initialises the 20 weeks long list_Of_Weeks for each room present in the University
+ * 
+ * @author Yajur Ahuja 2016121
+ * @author Shravika Mittal - 2016093
+ *
+ */
 public class actual_Room implements Serializable{
 	
 	private List<Week> list_Of_Weeks = new ArrayList<Week>(20);
 	private String name;
 	private int capacity;
 
-	public List<Week> getList_Of_Weeks() {
-		return list_Of_Weeks;
-	}
-
+	/**
+	 * 
+	 * Constructor for actual_Room.java
+	 * 
+	 * @param name of the room
+	 * @param capacity of the room
+	 */
 	public actual_Room(String name, int capacity) 
 	{
 		this.name = name;
 		this.capacity = capacity;
-		for(int i = 0 ; i<20 ; i++)
-		{
-			System.out.println("A");
-			this.list_Of_Weeks.add(new Week());
-		}
+		add_List_Of_Weeks(this.list_Of_Weeks);
 	}
 	
+	/**
+	 * default constructor for actual_Room.java
+	 */
 	public actual_Room() 
 	{
 		for(int i = 0 ; i<20 ; i++)
@@ -31,26 +41,49 @@ public class actual_Room implements Serializable{
 			this.list_Of_Weeks.add(new Week());
 		}
 	}
+	
+	/**
+	 * This is a getter function for lilst_Of_Weeks
+	 * @return list_Of_Weeks for each actual_Room(room) object
+	 */
+	public List<Week> getList_Of_Weeks() {
+		return list_Of_Weeks;
+	}
 
+	/**
+	 * This is the getter function for name of the room
+	 * @return name(String) of the room
+	 */
 	public String get_Name() {
 		return name;
 	}
 	
-
+	/**
+	 * This is the getter function for capacity of the room
+	 * @return capacity(int) of the room
+	 */
 	public int get_Capacity() {
 		return capacity;
 	}
 	
+	/**
+	 * 
+	 * This is for initialising the 20 weeks for each actual_Room object by adding new Week() objects
+	 * 
+	 * @param list_Of_Weeks 
+	 */
 	public void add_List_Of_Weeks(List<Week> list_Of_Weeks)
 	{
-		
+		for(int i = 0 ; i<20 ; i++)
+		{
+			list_Of_Weeks.add(new Week());
+		}
 	}
 	
-	public void add_Room(List<actual_Room> global_Room_List)
-	{
-		
-	}
-	
+	/**
+	 * This is the main method added for object creation testing purposes
+	 * @param unused
+	 */
 	public static void main(String[] args)
 	{
 		actual_Room obj = new actual_Room();
