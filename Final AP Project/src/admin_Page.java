6d64134691900ea0644a3f516024fe649b3810bc
yajur ahuja <yajur16121@iiitd.ac.in>
2017-11-16 13:48:41 +0530

    admin

--- Final AP Project/src/admin_Page.java
+++ Final AP Project/src/admin_Page.java
@@ -34,7 +34,8 @@ public class admin_Page extends Application
 		Button btn3 = new Button("Cancel Booking");
 		Button btn4 = new Button("Logout");
 		Button btn5 = new Button("Show Request");
-		Label l1 = new Label("Name: Admin");
+		Label l1 = new Label("ADMIN");
+		l1.getStyleClass().add("admin_Page_Heading");
 		Label l2 = new Label("");
 		
 		btn.setOnAction(e -> {new book_Room(2, admin_User).start(primaryStage);});
@@ -62,58 +63,71 @@ public class admin_Page extends Application
 		}});
 		
 		btn.setPrefHeight(80);
-		btn.setPrefWidth(200);
+		btn.setPrefWidth(400);
 		btn2.setPrefHeight(80);
-		btn2.setPrefWidth(200);
+		btn2.setPrefWidth(400);
 		btn3.setPrefHeight(80);
-		btn3.setPrefWidth(200);
-		btn4.setPrefHeight(80);
-		btn4.setPrefWidth(100);
+		btn3.setPrefWidth(400);
+		btn4.setPrefHeight(40);
+		btn4.setPrefWidth(200);
 		btn5.setPrefHeight(80);
-		btn5.setPrefWidth(200);
+		btn5.setPrefWidth(400);
 		
 		GridPane root = new GridPane();
+		root.setGridLinesVisible(true);
 		
-		HBox x = new HBox();
-		x.setAlignment(Pos.TOP_RIGHT);
-		
-	    root.setHgap(35);
-	    root.setVgap(15);
-		
-		root.getColumnConstraints().add(new ColumnConstraints(100));
-		root.getColumnConstraints().add(new ColumnConstraints(200));
-		root.getColumnConstraints().add(new ColumnConstraints(200));
-		
-		root.add(l1, 0, 0, 1, 1);
-		root.add(btn4, 3, 0, 1, 1);
-		root.add(l2, 0, 1, 1, 1);
-		root.add(btn, 2, 3, 1, 1);
-		root.add(btn2, 2, 4, 1, 1);
-		root.add(btn3, 2, 5, 1, 1);
-		root.add(btn5, 2, 6, 1, 1);
 		
 		Label l = new Label("IIIT-D");
-		l.setAlignment(Pos.TOP_LEFT);
+		l.setAlignment(Pos.CENTER_LEFT);
 		l.getStyleClass().add("labelIIITD");
 		
-		x.getChildren().addAll(l, btn4);
-		x.setSpacing(310);
+		HBox[] show = new HBox[4];
+		for(int i = 0; i < 4;  i++)
+		{
+			show[i] = new HBox();
+			show[i].setSpacing(150);
+		}
+		
+		
+		show[0].getChildren().addAll(l, btn4);
+		show[0].setSpacing(550);
+		show[0].setAlignment(Pos.TOP_CENTER);
+		show[0].setAlignment(Pos.CENTER);
+		show[1].getChildren().addAll(btn, btn2);
+		show[1].setAlignment(Pos.CENTER);
+		show[2].getChildren().addAll(btn3, btn5);	
+		show[2].setAlignment(Pos.CENTER);
+		show[3].getChildren().addAll(l1);	
+		show[3].setAlignment(Pos.CENTER);
+		
+	
+		
+		
+		
+	
 		
 		Button btn6 = new Button("Home");
 		
 		HBox x2 = new HBox();
 		x2.setSpacing(300);
 		x2.getChildren().addAll(btn6);
-		x2.setAlignment(Pos.TOP_LEFT);
+		x2.setAlignment(Pos.CENTER);
 		
 		VBox y = new VBox();
+		y.setSpacing(50);
 		y.setPadding(new Insets(20));
 		
-		y.getChildren().addAll(x, root);
-		y.setSpacing(30);
-		y.setStyle("-fx-background-color: #00DDDD");
+		VBox final_page = new VBox();
 		
-		Scene scene	= new Scene(y, 1000, 600);			
+		
+		
+		
+		y.getChildren().addAll(show[1], show[2], x2);
+		final_page.getStyleClass().add("background");
+		final_page.setSpacing(50);
+		final_page.setPadding(new Insets(50, 20, 20, 20));
+		final_page.getChildren().addAll(show[0],show[3], y);
+		Scene scene	= new Scene(final_page, 1000, 600);			
 		scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 		primaryStage.setScene(scene);		
 		primaryStage.show();		
