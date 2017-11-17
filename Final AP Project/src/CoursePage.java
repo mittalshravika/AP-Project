1a77c87e46fd1a0be6b36ee6fd60b3448f8cdb48
Shravika Mittal <shravika16093@iiitd.ac.in>
2017-11-18 01:00:00 +0530

    documentation

--- Final AP Project/src/CoursePage.java
+++ Final AP Project/src/CoursePage.java
@@ -1,44 +1,46 @@
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-//import javafx.scene.layout.StackPane;
 import javafx.scene.control.*;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox.*;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox.*;
-import javafx.scene.text.TextAlignment;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 
-public class CoursePage extends Application
-{
-	
+/**
+ * GUI displaying the course details of a course
+ * 
+ * @author Yajur
+ * @author Shravika
+ *
+ */
+public class CoursePage extends Application {
+
 	private Course object;
 	User current_User;
 
-	public CoursePage(Course obj, User student_User)
-	{
+	/**
+	 * Constructor
+	 * 
+	 * @param obj
+	 * @param student_User
+	 */
+	public CoursePage(Course obj, User student_User) {
 		object = obj;
 		current_User = student_User;
 	}
-	
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
+	public void start(Stage primaryStage) throws Exception {
 		Scene CourseView;
 		GridPane Course;
 
@@ -48,12 +50,9 @@ public class CoursePage extends Application
 		Course.setPadding(new Insets(20, 20, 20, 20));
 		Course.setVgap(8);
 		Course.setHgap(10);
-		
-		//Course.setGridLinesVisible(true);
 
 		Label title = new Label("Course Page");
 		title.getStyleClass().add("admin_page_heading");
-		
 
 		HBox x = new HBox();
 		x.getChildren().addAll(title);
@@ -63,13 +62,11 @@ public class CoursePage extends Application
 		Label Name = new Label("Course Name:");
 		Name.setMinWidth(Region.USE_PREF_SIZE);
 		Name.getStyleClass().add("Course");
-		//Name.setWrapText(true);
-		
+
 		GridPane.setConstraints(Name, 0, 0);
 		Label Name_D = new Label();
 		Name_D.setText(object.getCoursename());
 		Name_D.getStyleClass().add("Course");
-		//Name_D.setMinWidth(Region.USE_PREF_SIZE);
 		GridPane.setConstraints(Name_D, 1, 0);
 
 		Label Number = new Label("Course Number:");
@@ -79,7 +76,6 @@ public class CoursePage extends Application
 		Label Number_D = new Label();
 		Number_D.getStyleClass().add("Course");
 		Number_D.setText(object.getCoursecode());
-		//Number_D.setMinWidth(Region.USE_PREF_SIZE);
 		GridPane.setConstraints(Number_D, 1, 1);
 
 		Label Instructor = new Label("Instructor:");
@@ -89,7 +85,6 @@ public class CoursePage extends Application
 		Label Instructor_D = new Label();
 		Instructor_D.setText(object.getInstructorname());
 		Instructor_D.getStyleClass().add("Course");
-		//Instructor_D.setMinWidth(Region.USE_PREF_SIZE);
 		GridPane.setConstraints(Instructor_D, 1, 2);
 
 		Label Type = new Label("Type of Course:");
@@ -99,7 +94,6 @@ public class CoursePage extends Application
 		Label Type_D = new Label();
 		Type_D.setText(object.getCoursetype());
 		Type_D.getStyleClass().add("Course");
-		//Type_D.setMinWidth(Region.USE_PREF_SIZE);
 		GridPane.setConstraints(Type_D, 1, 3);
 
 		Label PCond = new Label("Post Conditions:");
@@ -107,12 +101,10 @@ public class CoursePage extends Application
 		PCond.getStyleClass().add("Course");
 		GridPane.setConstraints(PCond, 0, 4);
 		Label PCond_D = new Label();
-		
-		//PCond_D.setMinWidth(Region.USE_PREF_SIZE);
+
 		String postarr[] = object.getPostconditions().split(";");
 		String postconditions = "";
-		for(int i = 0 ; i<postarr.length ; i++)
-		{
+		for (int i = 0; i < postarr.length; i++) {
 			String str = postarr[i].replace("\"", "");
 			postconditions = postconditions + str + "\n";
 		}
@@ -128,137 +120,122 @@ public class CoursePage extends Application
 		GridPane.setConstraints(Timings, 0, 5);
 		Label Timings_D = new Label();
 		String timings = "";
-		if(!(object.getMon().equals("-")))
-		{
+		if (!(object.getMon().equals("-"))) {
 			String arr[] = object.getMon().split(";");
 			timings = timings + "Monday " + arr[0] + "\n";
 		}
-		if(!(object.getTue().equals("-")))
-		{
+		if (!(object.getTue().equals("-"))) {
 			String arr[] = object.getTue().split(";");
 			timings = timings + "Tuesday " + arr[0] + "\n";
 		}
-		if(!(object.getWed().equals("-")))
-		{
+		if (!(object.getWed().equals("-"))) {
 			String arr[] = object.getWed().split(";");
 			timings = timings + "Wednesday " + arr[0] + "\n";
 		}
-		if(!(object.getThurs().equals("-")))
-		{
+		if (!(object.getThurs().equals("-"))) {
 			String arr[] = object.getThurs().split(";");
 			timings = timings + "Thursday " + arr[0] + "\n";
 		}
-		if(!(object.getFri().equals("-")))
-		{
+		if (!(object.getFri().equals("-"))) {
 			String arr[] = object.getFri().split(";");
 			timings = timings + "Friday " + arr[0] + "\n";
 		}
 		Timings_D.setText(timings);
-		//Timings_D.setMinHeight(Region.USE_PREF_SIZE);
 		Timings_D.setWrapText(true);
 		Timings_D.getStyleClass().add("Course");
 		Timings_D.setPrefHeight(150);
 		GridPane.setConstraints(Timings_D, 1, 5);
 
-
+		/**
+		 * Returns back to the Search Course list of the student
+		 */
 		Button Back = new Button("Back");
-		Back.setOnAction(new EventHandler<ActionEvent>(){
+		Back.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				
+
 				new CourseList(current_User).start(primaryStage);
-				
+
 			}
-			
+
 		});
 		Button Register = new Button("Register Course");
-		
-		Register.setOnAction(new EventHandler<ActionEvent>()
-				{
 
-					@Override
-					public void handle(ActionEvent arg0) {
-						
-						String course_Name = object.getCoursename();
-						try {
-							App.deserialize("courselist");
-						} catch (ClassNotFoundException | IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
-						try {
-							if(current_User.newcourse_List.size()>0)
-							{
-								App.deserialize(current_User.getEmail_id()+"new");
-								System.out.println("A");
-							}
-						} catch (ClassNotFoundException | IOException e2) {
-							// TODO Auto-generated catch block
-							e2.printStackTrace();
-						}
-						
-						for(int i = 0 ; i<App.course_List.size() ; i++)
-						{
-							if(App.course_List.get(i).getCoursename().equals(course_Name))
-							{
-								Course obj = App.course_List.get(i);
-								current_User.newcourse_List.add(obj);
-								System.out.println("A");
-							}
-							if(App.course_List.get(i).getCoursename().split(";")[0].equals("TUT"))
-							{
-								if(App.course_List.get(i).getCoursename().split(";")[1].equals(course_Name))
-								{
-									Course obj = App.course_List.get(i);
-									current_User.newcourse_List.add(obj);
-									System.out.println("A");
-								}
-							}
-						}
-						
-						try {
-							App.serialize(current_User.getEmail_id()+"new", "register");
-						} catch (IOException e2) {
-							// TODO Auto-generated catch block
-							e2.printStackTrace();
-						}
-						
-						try {
-							App.serialize("courselist", "course");
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+		/**
+		 * Adds the course to the student time table
+		 */
+		Register.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+
+				String course_Name = object.getCoursename();
+				try {
+					App.deserialize("courselist");
+				} catch (ClassNotFoundException | IOException e) {
+					e.printStackTrace();
+				}
+
+				try {
+					if (current_User.newcourse_List.size() > 0) {
+						App.deserialize(current_User.getEmail_id() + "new");
+					}
+				} catch (ClassNotFoundException | IOException e2) {
+					e2.printStackTrace();
+				}
+
+				for (int i = 0; i < App.course_List.size(); i++) {
+					if (App.course_List.get(i).getCoursename().equals(course_Name)) {
+						Course obj = App.course_List.get(i);
+						current_User.newcourse_List.add(obj);
+					}
+					if (App.course_List.get(i).getCoursename().split(";")[0].equals("TUT")) {
+						if (App.course_List.get(i).getCoursename().split(";")[1].equals(course_Name)) {
+							Course obj = App.course_List.get(i);
+							current_User.newcourse_List.add(obj);
 						}
 					}
-			
-				});
+				}
+
+				try {
+					App.serialize(current_User.getEmail_id() + "new", "register");
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+
+				try {
+					App.serialize("courselist", "course");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		});
 
 		Label l = new Label("IIIT-D");
 		l.setAlignment(Pos.TOP_LEFT);
 		l.getStyleClass().add("labelIIITD");
-		
+
 		HBox x1 = new HBox();
 		x1.getChildren().addAll(Back, Register);
 		x1.setAlignment(Pos.BOTTOM_LEFT);
 		x1.setSpacing(600);
-		
-		Course.getChildren().addAll(Name, Name_D, Number, Number_D, Instructor, Instructor_D, Type, Type_D, PCond, PCond_D, Timings, Timings_D);
+
+		Course.getChildren().addAll(Name, Name_D, Number, Number_D, Instructor, Instructor_D, Type, Type_D, PCond,
+				PCond_D, Timings, Timings_D);
 		Course.getStyleClass().add("gridCourse");
-		y.getChildren().addAll(l, x, Course, x1); 
+		y.getChildren().addAll(l, x, Course, x1);
 		y.setSpacing(30);
 		y.setPadding(new Insets(20));
 		y.setAlignment(Pos.TOP_CENTER);
-		
+
 		y.getStyleClass().add("background");
 
-		CourseView = new Scene(y, 1100, 800);	
+		CourseView = new Scene(y, 1100, 800);
 		CourseView.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 		primaryStage.setScene(CourseView);
 		primaryStage.show();
-
-
 
 	}
 }
--- Final AP Project/src/CourseSearch.java
+++ Final AP Project/src/CourseSearch.java
@@ -15,86 +15,87 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.HPos;
 
-public class CourseSearch extends Application
-{
+/**
+ * GUI for search course
+ * 
+ * @author Yajur
+ * @author Shravika
+ *
+ */
+public class CourseSearch extends Application {
 	User current_User;
-	
-	public CourseSearch(User current_User)
-	{
+
+	/**
+	 * Constructor
+	 * @param current_User
+	 */
+	public CourseSearch(User current_User) {
 		this.current_User = current_User;
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
+	public void start(Stage primaryStage) throws Exception {
 
 		Scene CourseSearchScene;
 
 		primaryStage.setTitle("Classroom Booking System");
-		
-		
+
 		HBox[] show = new HBox[5];
-		
-		for(int i = 0; i < 5; i++)
-		{
+
+		for (int i = 0; i < 5; i++) {
 			show[i] = new HBox();
 			show[i].setSpacing(150);
 		}
-		
-		
 
-
-
-		//Title
 		Label Title = new Label("SEARCH");
 		Title.getStyleClass().add("admin_Page_Heading");
-		
-		//Search Field
+
 		TextField Search = new TextField();
-		
+
 		Search.setPrefWidth(900);
 
 		Button Search_Button = new Button("Search");
-	
-		
-		Search_Button.setOnAction(new EventHandler<ActionEvent>(){
+
+		/**
+		 * Creates a list of courses according to the keywords searched for by the student 
+		 * and aso filters them according to time clashes
+		 */
+		Search_Button.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
-				
-				try 
-				{
+
+				try {
 					App.deserialize("courselist");
 					String str = Search.getText();
 					search_Course obj = new search_Course(str, App.course_List, current_User);
 					App.serialize("courselist", "course");
 					Search.setText("");
 					new CourseList(current_User).start(primaryStage);
-				} catch (ClassNotFoundException | IOException e)
-				{
+				} catch (ClassNotFoundException | IOException e) {
 					e.printStackTrace();
 				}
-				
-				
+
 			}
-			
+
 		});
-		
+
 		Button btn3 = new Button("Back");
-		
-		
-		btn3.setOnAction(e -> {new student_Page(current_User).start(primaryStage);});
-		
-		
+
+		/**
+		 * Returns back to the student page
+		 */
+		btn3.setOnAction(e -> {
+			new student_Page(current_User).start(primaryStage);
+		});
+
 		Label l = new Label("IIIT-D");
-		//l.setAlignment(Pos.TOP_CENTER);
 		l.getStyleClass().add("labelIIITD");
-		
+
 		show[0].getChildren().addAll(l);
 		show[0].setAlignment(Pos.CENTER);
 		show[1].getChildren().addAll(Title);
@@ -105,22 +106,20 @@ public class CourseSearch extends Application
 		show[3].setAlignment(Pos.BOTTOM_CENTER);
 		show[4].getChildren().addAll(btn3);
 		show[4].setAlignment(Pos.CENTER_LEFT);
-		
-		
+
 		VBox y = new VBox();
 		y.setPadding(new Insets(20));
 		y.setSpacing(50);
 		y.setAlignment(Pos.TOP_CENTER);
-		for(int i = 0; i < 5; i++)
-		{
+		for (int i = 0; i < 5; i++) {
 			y.getChildren().add(show[i]);
 		}
-		
+
 		y.getStyleClass().add("background");
 		CourseSearchScene = new Scene(y, 1100, 800);
 		CourseSearchScene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 
-		primaryStage.setScene(CourseSearchScene);	
+		primaryStage.setScene(CourseSearchScene);
 		primaryStage.show();
 	}
 }
